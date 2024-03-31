9ec48cd999911be11f4e0c751102febc24fbf824
hzume <yota040@gmail.com>
2024-03-31 15:59:06 +0000

    a

--- init.sh
+++ init.sh
@@ -4,12 +4,15 @@ mkdir /kaggle/input/pii-detection-removal-from-educational-data
 mkdir /kaggle/input/huggingfacedebertav3variants
 mkdir /kaggle/input/piimetric
 mkdir /kaggle/input/fix-punctuation-tokenization-external-dataset
+mkdir /kaggle/input/pii-mixtral8x7b-generated-essays
 
 kaggle competitions download -c pii-detection-removal-from-educational-data
 kaggle datasets download -d verracodeguacas/huggingfacedebertav3variants
 kaggle datasets download -d nischaydnk/piimetric
 kaggle kernels output valentinwerner/fix-punctuation-tokenization-external-dataset -p /kaggle/input/fix-punctuation-tokenization-external-dataset
+kaggle datasets download -d mpware/pii-mixtral8x7b-generated-essays
 
 unzip pii-detection-removal-from-educational-data.zip -d /kaggle/input/pii-detection-removal-from-educational-data
 unzip huggingfacedebertav3variants.zip -d /kaggle/input/huggingfacedebertav3variants
 unzip piimetric.zip -d /kaggle/input/piimetric
+unzip pii-mixtral8x7b-generated-essays.zip -d /kaggle/input/pii-mixtral8x7b-generated-essays
